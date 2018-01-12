4d29c153ac468bad0b421364f817da1394b0df82
Vernesti <33489350+Vernesti@users.noreply.github.com>
2018-01-12 12:41:48 +0200

    Значення з тесту виводиться в консоль + картинка з чуваками "О компании"
    //

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-	 
-	 $('#siteform input:radio:checked').each(function(){
-		alert($(this).val());
-	});
+		
+		$('.submit').click(function () {
+	 		$("input:radio:checked").each(function () {
+		 		console.log($(this).attr('name') + ' ' + $(this).val());
+	 		})
+		 		if ($("textarea").val().length > 0) {
+			 			$("textarea").each(function () {
+				 		console.log($(this).attr('name') + ' ' + $(this).val());
+			 		})
+	 			}
+	 		
+	 	})
+
+
+
