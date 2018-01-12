00eb7fbca7c8ad9bf590befbb419d5e5225a2ea4
Vernesti <33489350+Vernesti@users.noreply.github.com>
2018-01-12 21:23:48 +0200

    Values of tests v0.2
    //

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 		
-		$('.submit').click(function () {
-	 		$("input:radio:checked").each(function () {
-		 		console.log($(this).attr('name') + ' ' + $(this).val());
-	 		})
-		 		if ($("textarea").val().length > 0) {
-			 			$("textarea").each(function () {
-				 		console.log($(this).attr('name') + ' ' + $(this).val());
-			 		})
-	 			}
-	 		
-	 	})
+// $('.submit').click(function () {
+// 		$("input:radio:checked").each(function () {
+//  		console.log($(this).attr('name') + ' ' + $(this).val());
+// 		})
+//  		if ($("textarea").val().length > 0) {
+// 	 			$("textarea").each(function () {
+// 		 		console.log($(this).attr('name') + ' ' + $(this).val());
+// 	 		})
+// 			}
+		
+// 	})
 
 
+
+$('.submit').click(function () {
+ 	var arrayInp = $("input:radio:checked").serializeArray();
+ 	var arrayTextarea = $("textarea").serializeArray();
+		console.log(arrayInp);
+		console.log(arrayTextarea);
+		
+	var jsonSend = $.post("../json/test.json",arrayInp,)
+		.success(function() { alert("Успешное выполнение"); })
+		.error(function() { alert("Ошибка выполнения"); })
+		.complete(function() { alert("Завершение выполнения"); });
+})
 
