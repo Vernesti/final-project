bc0599894f9f88964e9cb152096ea403b8e64d01
Vernesti <33489350+Vernesti@users.noreply.github.com>
2018-01-29 11:28:01 +0200

    Респонс меню + робота меню на JS
    //

---
+++ pages/choose-test/script.js
@@ -0,0 +1,11 @@
+$('#menu-resp__button').click(function () {
+	$('.header-list-resp').toggleClass('show-block');
+})
+
+$('#tests-all').click(function () {
+	$('#tests-all-drop').toggleClass('show-block');
+})
+
+$('#tests-programing').click(function () {
+	$('#tests-programing-drop').toggleClass('show-block');
+})
